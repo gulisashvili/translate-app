1638f31b843cae76cbd182723a742ffb9e65c222
Levan Gulisashvili <levan.gulisashvili@gmail.com>
2014-05-13 16:53:56 +0400

    added translate.ge api and data is returned with success

--- translate_api.rb
+++ translate_api.rb
@@ -1,3 +1,17 @@
-module TranslateApi
+require "net/http"
+require "json"
 
+module TranslateApi
+  class Translator
+    def get_data(word)
+      uri = URI("http://translate.ge/Main/Translate?text=#{word}&lang=en&")
+      res = Net::HTTP.get_response(uri)
+      res = JSON.parse(res.body)
+      response = res[0]["Text"]
+    end 
+  end 
 end
+
+tr = TranslateApi::Translator.new
+
+puts tr.get_data("girl")
