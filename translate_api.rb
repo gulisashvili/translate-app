0b2a2ecf01b1bf23a7633d4e430341266e33c12c
Levan Gulisashvili <levan.gulisashvili@gmail.com>
2014-05-13 22:41:17 +0400

    bugs bugs bugs

--- translate_api.rb
+++ translate_api.rb
@@ -3,15 +3,19 @@ require "json"
 
 module TranslateApi
   
-  class Translator
-    
+  class Translator    
     def get_data(word)
       uri = URI("http://translate.ge/Main/Translate?text=#{word}&lang=en&")
       res = Net::HTTP.get_response(uri)
       res = JSON.parse(res.body)
-      response = { res[0]["ReqText"] => res[0]["Text"] }
+      data = { res[0]["ReqText"] => res[0]["Text"]}
     end 
   
   end 
+
+
+# a = Translator.new
+
+# puts a.get_data("boy")
 
 end
